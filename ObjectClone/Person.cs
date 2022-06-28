a4c1f009048a21519e7a67928d3068c4f8434868
Ani Mkrtchyan <mkrtchyan.ani@outlook.com>
2022-06-29 00:49:32 +0400

    Add project files.

---
+++ ObjectClone/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectClone
+{
+    public class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        // Copy Constructor
+        public Person(Person other)
+        {
+            this.Name = other.Name;
+            this.Age = other.Age;
+        }
+        #region ICloneable Members
+        // Type safe Clone
+        public Person Clone() 
+
+        { 
+            return new Person(this);
+        }
+      
+        // ICloneable implementation
+        object ICloneable.Clone()
+        {
+            return Clone();
+        }
+        #endregion
+    }
+}
---
+++ ObjectClone/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ObjectClone
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Person bob = new Person("Bob", 25);
+            Person bob_clone = bob.Clone();
+            Debug.Assert(bob_clone.Name == bob.Name);
+            bob.Age = 56;
+            Debug.Assert(bob.Age != bob.Age);
+            Console.WriteLine(bob.Age);
+            Console.WriteLine(bob.Name);
+        }
+    }
+}
+
